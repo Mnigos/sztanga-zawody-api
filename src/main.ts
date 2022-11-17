dee6092e394671fff2643bf212cb581b65650afd
Igor <makowskiigor@gmail.com>
2022-11-17 12:13:00 +0100

    fix: error while starting app

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,21 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 import { AppModule } from './app.module'
 
-const app = await NestFactory.create(AppModule)
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
 
-app.enableCors()
-app.useGlobalPipes(new ValidationPipe())
+  app.enableCors()
+  app.useGlobalPipes(new ValidationPipe())
 
-const config = new DocumentBuilder()
-  .setTitle('Sztanga API')
-  .setVersion('1.0')
-  .addTag('sztanga')
-  .build()
+  const config = new DocumentBuilder()
+    .setTitle('Sztanga API')
+    .setVersion('1.0')
+    .addTag('sztanga')
+    .build()
 
-const document = SwaggerModule.createDocument(app, config)
-SwaggerModule.setup('api', app, document)
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
 
-await app.listen(4000)
+  await app.listen(4000)
+}
+bootstrap()
